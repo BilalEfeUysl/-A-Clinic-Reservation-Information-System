6b501b8973d33452ad85d8d9789f127597d43f5e
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2024-12-31 00:40:33 +0300

    Editing the Person Class

---  A Clinic Reservation Information System/src/Reservation_System/Person.java
+++  A Clinic Reservation Information System/src/Reservation_System/Person.java
@@ -1,7 +1,23 @@
 
 package Reservation_System;
 
+import java.io.Serializable;
 
-public class Person {
+
+public class Person implements Serializable {
     
+    private static final long serialVersionUID = 1L;
+    
+    private String name;
+    private final long national_id;
+    
+    public Person(String name,Long national_id){
+        this.name = name;
+        this.national_id = national_id;
+    }
+    
+    @Override
+    public String toString(){
+        return "Name: " + this.name + " , National ID: " + String.valueOf(this.national_id); 
+    }
 }
