3b812719dad79d2c909da3fe96ae10f97fef420e
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2025-01-03 01:38:50 +0300

    Editing the Section Class

---  A Clinic Reservation Information System/src/Reservation_System/Section.java
+++  A Clinic Reservation Information System/src/Reservation_System/Section.java
@@ -1,7 +1,49 @@
 
 package Reservation_System;
 
+import java.io.Serializable;
+import java.util.LinkedList;
 
-public class Section {
+
+public class Section implements Serializable{
+    private final int id;
+    private String name;
+    private LinkedList<Doctor> doctors;
+
+    public Section(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.doctors = new LinkedList<>();
+    }
+    
+    public void listDoctor(){
+        for (Doctor doctor : doctors) {
+            System.out.println(doctor);
+        }
+    }
+
+    public Doctor getDoctors(int diploma_id) {
+        for (Doctor doctor : doctors) {
+            if(doctor.getDiploma_id() == diploma_id){
+                return doctor;
+            }
+        }
+        return null;
+    }
+    
+    public void addDoctor(Doctor doctor){
+        doctors.add(doctor);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+    
     
 }
