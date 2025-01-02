68e825ef93016eae5fc3371bf7d34dd414290861
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2025-01-03 01:36:32 +0300

    Editing the Doctor Class

---  A Clinic Reservation Information System/src/Reservation_System/Doctor.java
+++  A Clinic Reservation Information System/src/Reservation_System/Doctor.java
@@ -2,6 +2,27 @@
 package Reservation_System;
 
 
-public class Doctor {
+public class Doctor extends Person {
+    private final int diploma_id;
+
+    public Doctor(String name,long national_id,int diploma_id){
+        super(name,national_id);
+        this.diploma_id = diploma_id;
+    }
     
+    public Schedule getSchedule(){
+        return null;
+    }
+    
+    public int getDiploma_id() {
+            return diploma_id;
+    }
+    
+    @Override
+    public String toString(){
+        return "Doctor Name: " + getName() + ", National ID: " + getNational_id() + ", Diploma ID: " + this.diploma_id;
+    }
+
+    
+
 }
