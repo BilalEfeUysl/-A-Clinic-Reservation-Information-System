26e0ddc7c42e6b4080b27038f15414de2634bdf1
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2025-01-03 01:40:06 +0300

    Editing the CRS Class

---  A Clinic Reservation Information System/src/Reservation_System/CRS.java
+++  A Clinic Reservation Information System/src/Reservation_System/CRS.java
@@ -1,7 +1,26 @@
 
 package Reservation_System;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+
 
 public class CRS {
+    private HashMap<Long,Patient> patients;
+    private LinkedList<Rendezvous> rendezvous;
+    private HashMap<Integer,Hospital> hospitals;
     
+    public boolean makeRandezvous(long patientID,int HospitalID,int sectionID,int diplomaID,Date desiredDate){
+        return true;
+    }
+    
+    public void saveTablesToDisk(String fullPath){
+        
+    }
+    
+    public void loadTablesToDisk(String fullPath){
+        
+    }
 }
