e3147d8855beb26c3203abfff8b3d8dc0202516d
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2025-01-03 01:38:04 +0300

    Editing the Schedule Class

---  A Clinic Reservation Information System/src/Reservation_System/Schedule.java
+++  A Clinic Reservation Information System/src/Reservation_System/Schedule.java
@@ -1,7 +1,22 @@
 
 package Reservation_System;
 
+import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedList;
 
-public class Schedule {
+
+public class Schedule implements Serializable{
+    private LinkedList<Rendezvous> sessions;
+    private int maxPatientPerDay;
     
+    public Schedule(int maxPatientPerDay){
+        this.maxPatientPerDay = maxPatientPerDay;
+        
+        this.sessions = new LinkedList<>();
+    }
+    
+    public boolean addRendezvous(Patient p, Date desired){
+        return true;
+    }
 }
