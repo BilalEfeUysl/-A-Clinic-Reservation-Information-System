2b9e033a4b7b56b12b68309dc1cf255174df5919
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2025-01-03 01:39:22 +0300

    Editing the Hospital Class

---  A Clinic Reservation Information System/src/Reservation_System/Hospital.java
+++  A Clinic Reservation Information System/src/Reservation_System/Hospital.java
@@ -1,7 +1,44 @@
 
 package Reservation_System;
 
+import java.io.Serializable;
+import java.util.LinkedList;
 
-public class Hospital {
+
+public class Hospital implements Serializable{
+    private final int id;
+    private String name;
+    private LinkedList<Section> sections;
+
+    public Hospital(int id, String name) {
+        this.id = id;
+        this.name = name;
+        
+        this.sections = new LinkedList<>();
+    }
+
+    public Section getSections(int id) {
+        for (Section section : sections) {
+            if(section.getId() == id){
+                return section;
+            } 
+        }
+        
+        return null;
+    }
+    
+    public Section getSections(String name) {
+        for (Section section : sections) {
+            if(section.getName() == name){
+                return section;
+            } 
+        }
+        
+        return null;
+    }
+    
+    public void addSection(Section section){
+        sections.add(section);
+    }
     
 }
