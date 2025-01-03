086a9b1545ed5360a90b47c8bf4917d20879f0d6
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2025-01-03 21:10:05 +0300

    Editing the Schedule Class Added addRendezvous method

---  A Clinic Reservation Information System/src/Reservation_System/Rendezvous.java
+++  A Clinic Reservation Information System/src/Reservation_System/Rendezvous.java
@@ -6,4 +6,16 @@ import java.util.Date;
 
 public class Rendezvous {
     private Date dateTime;
+    private Patient patient;
+
+    public Rendezvous(Patient patient , Date dateTime) {
+        this.dateTime = dateTime;
+        this.patient = patient;
+    }
+    
+    public Date getDate(){
+        return this.dateTime;
+    }
+    
+    
 }
---  A Clinic Reservation Information System/src/Reservation_System/Schedule.java
+++  A Clinic Reservation Information System/src/Reservation_System/Schedule.java
@@ -2,6 +2,7 @@
 package Reservation_System;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedList;
 
@@ -17,6 +18,26 @@ public class Schedule implements Serializable{
     }
     
     public boolean addRendezvous(Patient p, Date desired){
+        int sameDayCount = 0;
+        Calendar desiredCal = Calendar.getInstance();
+        desiredCal.setTime(desired);
+        
+        for (Rendezvous session : sessions) {
+            Calendar existingCal = Calendar.getInstance();
+            existingCal.setTime(session.getDate());
+            
+            if(existingCal.get(Calendar.YEAR) == desiredCal.get(Calendar.YEAR) &&
+               existingCal.get(Calendar.DAY_OF_YEAR) == desiredCal.get(Calendar.DAY_OF_YEAR)){
+                sameDayCount++;
+            }
+        }
+        
+        if(sameDayCount >= maxPatientPerDay){
+            return false;
+        }
+        
+        Rendezvous newRendezvous = new Rendezvous(p, desired);
+        sessions.add(newRendezvous);
         return true;
     }
 }
